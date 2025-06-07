2f6255302417bf0c8ab5f94e1757a983061cc962
sudhanshu agariya <sudhanshu.agariya@nextuple.com>
2025-06-07 12:31:43 +0530

    first commit

---
+++ src/main/java/com/example/unifize/UnifizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.unifize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unifize/UnifizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unifize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
